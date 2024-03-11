4cb8f53bb222d9e8ad72aa511a5189e6189aef4d
Juan Blanco <juanblancodev@gmail.com>
2024-03-10 20:05:00 -0700

    Github Link updated

--- src/configs/config.js
+++ src/configs/config.js
@@ -1,4 +1,4 @@
 export default {
   URL_REACT_WEBSITE: "https://es.react.dev/",
-  URL_GITHUB_PROFILE: "https://github.com/JuanBlancodev"
+  URL_GITHUB_PROFILE: "https://github.com/JuanBlancodev?tab=repositories"
 }
