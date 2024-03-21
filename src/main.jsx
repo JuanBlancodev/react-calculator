cdcf4d170198d58caf4ac9b7f7b3cedc01055ab3
Juan Blanco <juanblancodev@gmail.com>
2024-03-21 01:59:46 -0700

    main.css name changed to index.css

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import ReactDOM from 'react-dom/client'
 import App from './components/App.jsx'
-import './styles/main.css'
+import './styles/index.css'
 
 import { GlobalContextProvider } from './context/GlobalContext.jsx'
 
