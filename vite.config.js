f2728704d0a19c33ef64163a1820c6fa3c182f40
Juan Blanco <juanblancodev@gmail.com>
2024-03-22 00:09:15 -0700

    terser added

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import terser from '@rollup/plugin-terser'
 import postcss from '@vituum/vite-plugin-postcss'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     react(),
+    terser(),
     postcss()
   ],
   base: "https://JuanBlancodev.github.io/react-calculator"
