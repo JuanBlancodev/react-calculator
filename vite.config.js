295dd82d85be7815e6e2167b5146ae9ade835a21
Juan Blanco <juanblancodev@gmail.com>
2024-03-22 00:03:33 -0700

    postcss added

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import postcss from '@vituum/vite-plugin-postcss'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  build: {
-    sourcemap: true
-  },
+  plugins: [
+    react(),
+    postcss()
+  ],
   base: "https://JuanBlancodev.github.io/react-calculator"
 })
